f07577eb2034e2789395064008625d4533d5ef6a
huangwl <410488219@qq.com>
2022-04-24 18:19:25 +0800

    merge query/form/json params

--- src/main/kotlin/ez/rest_db_proxy/RoutingContextEx.kt
+++ src/main/kotlin/ez/rest_db_proxy/RoutingContextEx.kt
@@ -2,13 +2,16 @@ package ez.rest_db_proxy
 
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.impl.ParsableMIMEValue
 
 /**
  * Merge queryParams and body into a json object. All params in query or form will be treated as strings
  */
 fun RoutingContext.paramsAsJson(): JsonObject {
   val params = queryParams().toJson()
-  val bodyJson = when (parsedHeaders().contentType()?.subComponent()) {
+  val contentType = parsedHeaders().contentType() as ParsableMIMEValue
+  contentType.forceParse()
+  val bodyJson = when (contentType.subComponent()) {
     "json" -> bodyAsJson
     "x-www-form-urlencoded" -> request().formAttributes().toJson()
     else -> null
